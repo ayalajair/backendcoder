ce1ade066544526b063217bc943523f8e2b515a0
ayalajair <ayalajair@gmail.com>
2023-03-27 19:58:29 -0300

    se crearon deleteProduct y updateProduct

--- desafio.js
+++ desafio.js
@@ -48,22 +48,79 @@ class ProductManager {
             //Pusheo el nuevo producto en products y lo escribo en el Json
             products.push(newProduct);
             await fs.promises.writeFile (this.path,JSON.stringify(products),'utf8');
-        }catch (err){
-            console.log (Error)
+        }catch (error){
+            console.log (error);
         }
     }
 
-    getProducts() {
-        return this.products;
+    getProducts = async () => {
+        try{
+            const products = await this.loadProducts();
+            console.log (products)
+            return products;
+        } catch (error){
+            console.log (error)
+        }
     }
 
-    getProductById(id) {
-        const product = this.products.find((p) => p.id === id);
-        if (!product) {
-            console.log("No se han encontrado productos con ese ID");
-            return;
+    getProductById = async (id) => {
+        try {
+            const products = await this.loadProducts()
+            const product = products.find((p) => p.id === id);
+            if (!product) {
+                console.log("No se han encontrado productos con ese ID");
+                return;
+            }
+            console.log (product)
+            return product;
         }
-        return product;
+        catch (error) {
+            console.log (error)
+        }
+    }
+
+    updateProduct = async (id, updatedProduct) => {
+        try {
+            const products = await this.loadProducts();
+            const productIndex = products.findIndex((product) => product.id === id);
+            if (productIndex === -1) {
+                console.log("No se han encontrado productos con ese ID");
+                return;
+            }
+            const product = products.find((p) => p.id === id);
+            if (!product) {
+                console.log("No se han encontrado productos con ese ID");
+                return;
+            }
+            const updatedProductWhitId = {
+                id ,
+                ...updatedProduct,
+            }
+            product[productIndex] = updatedProductWhitId;
+            console.log (updatedProduct)
+            await fs.promises.writeFile (this.path,JSON.stringify(products),'utf8');
+        } catch (error) {
+            console.log (error)
+        }
+    }
+
+    deleteProduct = async (id) => {
+        try {
+
+            const products = await this.loadProducts();
+            const productIndex = products.findIndex((product) => product.id === id);
+            if (productIndex === -1) {
+                console.log ("No se han encontrados productos con ese ID")
+                return null;
+            }
+            const deletedProduct = products.splice(productIndex, 1)[0];
+            await fs.promises.writeFile (this.path,JSON.stringify(products),'utf8');
+            console.log ("El producto ha sido borrado con éxito")
+            return deletedProduct;
+        } catch (error) {
+            console.log (error)
+        }
+        
     }
 }
 
@@ -74,14 +131,17 @@ const productList = new ProductManager(path);
 //Agrego producto 
 productList.addProduct({title:'Producto', description: 'Descripción', price: 100, thumbnail: 'thumbnail', code:'123456', stock: 100});
 //Agrego producto con el mismo código
-//productList.addProduct({title:'Producto', description: 'Descripción', price: 100, thumbnail: 'thumbnail', code:'123456', stock: 100});
+productList.addProduct({title:'Producto', description: 'Descripción', price: 100, thumbnail: 'thumbnail', code:'123456', stock: 100});
 //Agrego producto incompleto
-//productList.addProduct({title:'Producto2', description: 'Descripción', price: 100, thumbnail: 'thumbnail', code:'123456'});
+productList.addProduct({title:'Producto2', description: 'Descripción', price: 100, thumbnail: 'thumbnail', code:'123456'});
 //Agrego 2do producto
-//productList.addProduct({title:'Producto3', description: 'Descripción', price: 100, thumbnail: 'thumbnail', code:'123457', stock: 100});
+productList.addProduct({title:'Producto3', description: 'Descripción', price: 100, thumbnail: 'thumbnail', code:'123457', stock: 100});
 //Busco producto con Id 2
-//console.log(productList.getProductById(2));
+productList.getProductById(2);
 //Busco producto con Id 10
-//productList.getProductById(10);
+productList.getProductById(10);
 //Pido lista completa de productos
-//console.log(productList.getProducts());
+productList.getProducts();
+//Modifico un producto
+let newProduct = {title:'Producto4', description: 'Descripción2', price: 200, thumbnail: 'thumbnail', code:'123456', stock: 100}
+productList.updateProduct(2, newProduct )
