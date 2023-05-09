e42f86381f40c14f70c932f3fe94e54c86464405
ayalajair <ayalajair@gmail.com>
2023-05-08 21:49:19 -0300

    Se modificó el config

--- src/config/configServer.js
+++ src/config/configServer.js
@@ -1,7 +1,7 @@
 const{connect} = require ('mongoose')
 module.exports = {
     connectDB: ()=>{
-        connect('mongodb://localhost:27017/ecommerce')
+        connect('mongodb+srv://jairayala:coder123456@cluster0.aa9hemg.mongodb.net/')
         console.log('Database connected')
     }
 }
