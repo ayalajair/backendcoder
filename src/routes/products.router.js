18748acb06b6b226d5115048341287bc3b01b5ab
Jair Ayala <82057504+ayalajair@users.noreply.github.com>
2023-05-15 17:06:35 +0000

    se agregó validacion

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -1,9 +1,23 @@
 const { Router } =  require('express')
 const ProductManager = require ('../DAO/db/products.Manager.Mongo')
+const Ajv = require('ajv')
 
 const router = Router();
 const products = new ProductManager()
+const ajv = new Ajv()
 
+const schema = {
+    type: 'object',
+    properties: {
+        limit: { type: 'integer', minimum: 1, maximum: 100 },
+        page: { type: 'integer', minimum: 1, maximum: 100 },
+        priceSort: { type: 'string', enum: ['asc', 'desc'] },
+        category: { type: 'string'},
+        availability: { type: 'string'}
+    }
+}
+
+const validate = ajv.compile(schema);
 
 //-----------------GET------------------------------------------
 router.get('/', async (req,res)=>{
@@ -20,12 +34,15 @@ router.get('/', async (req,res)=>{
         if(availability){
             query = {...query, availability}
         }
-        const sort = null
-        if(priceSort==='asc'){
-            sort = {price:1}
-        }    
-        if(priceSort==='desc'){
-            sort = {price:-1}
+
+        const sort = priceSort ? { price: priceSort === 'asc' ? 1 : -1 } : null;
+
+        if (!validate(req.query)) {
+            return res.status(400).send({
+              status: 'error',
+              message: 'Invalid query parameters',
+              details: validate.errors
+            });
         }
 
         
