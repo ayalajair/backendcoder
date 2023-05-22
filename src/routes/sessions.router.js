b83ec137f2a1d233de543780023f05674456c567
Jair Ayala <82057504+ayalajair@users.noreply.github.com>
2023-05-22 17:03:12 +0000

    se crea el endpoint /login

--- src/routes/sessions.router.js
+++ src/routes/sessions.router.js
@@ -23,7 +23,25 @@ router.post('/register', async (req, res)=>{
 router.post('/login', async (req, res)=>{
     try {
         const {email, password} = req.body
-        const user = 
+        if (email === 'adminCoder@coder.com' && password === 'adminCod3r123'){
+            const user = {
+                email: 'adminCoder@coder.com'
+                password: 'adminCod3r123'
+                role: 'admin'
+            }
+            return res.send({success: true, payload:  user, message: 'Bienvenido admin'})
+        }
+        const user = await users.getUserByEmail(email)
+        if(!user){
+            return res.status(401).send({message: 'Credenciales inválidas'})
+        } 
+        if(user.password !== password){
+            return res.status(401).json({ message: 'Credenciales inválidas' })
+        } user.role =  'user'
+
+        req.session.user = user;
+        res.status(200).json({ message: 'Inicio de sesión exitoso' });
+
     } catch (error) {
         return res.status(400).send(error)
     
