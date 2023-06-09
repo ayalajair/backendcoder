2bb0bb8923945f82f7cdf3ac78e103eec88a9797
ayalajair <ayalajair@gmail.com>
2023-06-09 19:57:32 -0300

    Se implementó current

--- src/routes/sessions.router.js
+++ src/routes/sessions.router.js
@@ -3,19 +3,13 @@ const {login, register} = require ('../controllers/sessions.controller')
 const {UsersManagerMongo} = require('../DAO/db/users.Manager.Mongo');
 const passport = require('passport');
 const {generateToken} = require('../utils/generateTokenJWT')
+const {passportAuth} = require('../config/passport.JWT/passport.auth')
+const {authorization} = require('../config/passport.JWT/passport.authorization')
 
 const router = Router();
 
 
 
-//------Logout-------
-router.get('/logout', (req, res) => {
-    // Limpiar la sesión
-    req.session.destroy();
-
-    // Redirigir al usuario a la página de inicio de sesión u otra página
-    return res.redirect('/');
-});
 
 //------Succesfull register-------
 router.post('/register', register)
@@ -24,12 +18,29 @@ router.post('/register', register)
 
 router.post('/login', login)
 
+//------Current-----------------
+
+router.get('/current', passportAuth('jwt'), (req, res) => {
+    user = req.user
+    console.log(user)
+    
+    res.send(user)
+})
+
+//------Logout-------
+// router.get('/logout', (req, res) => {
+//     // Limpiar la sesión
+//     req.session.destroy();
+
+//     // Redirigir al usuario a la página de inicio de sesión u otra página
+//     return res.redirect('/');
+// });
 //------Failed register-------
 
-router.get('/failRegister', (req, res) => {
-    console.log('Registro fallido')
-    res.send({status: 'error', error: 'Registro fallido'})
-})
+// router.get('/failRegister', (req, res) => {
+//     console.log('Registro fallido')
+//     res.send({status: 'error', error: 'Registro fallido'})
+// })
 
 
 //------Failed login-------
@@ -43,12 +54,11 @@ router.get('/github', passport.authenticate('github', {scope:['user: email']}))
 
 
 router.get('/githubcallback', passport.authenticate('github', {failureRedirect: '/failLogin'}), async (req, res)=>{
-    console.log(req.user)
-    const token = generateToken(req.user)
+    req.session.user = req.user
     console.log('Login exitoso')
-    res.cookie('cookieToken',token,{"maxAge": 3600000, httpOnly: true}).send({status: 'success', token})
-    // res.redirect('/products')
+    res.redirect('/products')
 })
+
 
 
 module.exports = router
