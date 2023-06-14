9f654ab95370ff8b9ff0225f60af37e9bce666df
Santiago Cerrutti <cerrutti.js@gmail.com>
2023-06-14 10:24:27 -0300

    correccion segunda practica integradora

--- src/routes/sessions.router.js
+++ src/routes/sessions.router.js
@@ -1,32 +1,38 @@
-const {Router} = require('express');
-const {login, register} = require ('../controllers/sessions.controller')
-const {UsersManagerMongo} = require('../DAO/db/users.Manager.Mongo');
-const passport = require('passport');
-const {generateToken} = require('../utils/generateTokenJWT')
-const {passportAuth} = require('../config/passport.JWT/passport.auth')
-const {authorization} = require('../config/passport.JWT/passport.authorization')
+const { Router } = require('express')
+const { login, register } = require('../controllers/sessions.controller')
+const { UsersManagerMongo } = require('../DAO/db/users.Manager.Mongo')
+const passport = require('passport')
+const { generateToken } = require('../utils/generateTokenJWT')
+const { passportAuth } = require('../config/passport.JWT/passport.auth')
+const {
+    authorization,
+} = require('../config/passport.JWT/passport.authorization')
 
-const router = Router();
-
-
-
+const router = Router()
 
 //------Succesfull register-------
-router.post('/register', register)
+router.post(
+    '/register',
+    // @fix ES NECESARIO ENVIAR ESTE OPTIONS PARA DESACTIVAR LA SESSION EN PASSPORT
+    passportAuth('register', { session: false }),
+    register
+)
 
 //------Succesfull login-------
 
-router.post('/login', login)
+router.post('/login', passportAuth('login', { session: false }), login)
 
 //------Current-----------------
 
-router.get('/current',
-    passportAuth('jwt'),
+router.get(
+    '/current',
+    passportAuth('jwt', { session: false }),
     authorization('user'),
     (req, res) => {
-    user = req.user
-    res.send(user)
-})
+        user = req.user
+        res.send(user)
+    }
+)
 
 //------Logout-------
 // router.get('/logout', (req, res) => {
@@ -43,23 +49,38 @@ router.get('/current',
 //     res.send({status: 'error', error: 'Registro fallido'})
 // })
 
-
 //------Failed login-------
 router.get('/failLogin', (req, res) => {
     console.log('Login fallido')
-    res.send({status: 'error', error: 'Login fallido'})
+    res.send({ status: 'error', error: 'Login fallido' })
 })
 
 //------Login with GitHub-------
-router.get('/github', passport.authenticate('github', {scope:['user: email']}))
+router.get(
+    '/github',
+    passport.authenticate('github', { scope: ['user: email'], session: false })
+)
 
-
-router.get('/githubcallback', passport.authenticate('github', {failureRedirect: '/failLogin'}), async (req, res)=>{
-    req.session.user = req.user
-    console.log('Login exitoso')
-    res.redirect('/products')
-})
-
-
+router.get(
+    '/githubcallback',
+    passport.authenticate('github', {
+        failureRedirect: '/failLogin',
+        session: false,
+    }),
+    // @fix AL VOLVER DE GITHUB, SE DEBE CREAR EL TOKEN CON LOS DATOS DEL USUARIO
+    async (req, res) => {
+        // req.session.user = req.user;
+        if (req.user) {
+            console.log('req.user', req.user)
+            const token = generateToken(req.user)
+            res.cookie('cookieToken', token, {
+                maxAge: 3600000,
+                httpOnly: true,
+            })
+        }
+        console.log('Login exitoso')
+        res.redirect('/products')
+    }
+)
 
 module.exports = router
