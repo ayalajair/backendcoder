6ddb2f8507f7ab187668e21340560507ed087055
Jair Ayala <ayalajair@gmail.com>
2023-07-20 11:30:38 +0000

    se modificaron los errores

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -4,25 +4,25 @@ exports.errorHandler = (error, req, res, next) => {
     console.log(error.cause)
     switch (error.code) {
         case EError.ROUTING_ERROR:
-            return send ({status: 'error', error: error.name})
+            return res.status(400).send ({status: 'error', error: error.name})
             break
         case EError.NOT_FOUND:
-            return send ({status: 'error', error: error.name})
+            return res.status(404).send ({status: 'error', error: error.name})
             break
         case EError.DATABASE_ERROR:
-            return send ({status: 'error', error: error.name})
+            return res.status(500).send ({status: 'error', error: error.name})
             break
         case EError.INVALID_TYPE_ERROR:
-            return send ({status: 'error', error: error.name})
+            return res.status(400).send ({status: 'error', error: error.name})
             break
         case EError.UNAUTHORIZED:
-            return send ({status: 'error', error: error.name})
+            return res.status(401).send ({status: 'error', error: error.name})
             break
         case EError.VALIDATION_ERROR:
-            return send ({status: 'error', error: error.name})
+            return res.status(400).send ({status: 'error', error: error.name})
             break
         default: 
-            return send ({status: 'error', error: 'Unhandled error'})
+            return res.status(500).send ({status: 'error', error: 'Unhandled error'})
             break
         
     }       
